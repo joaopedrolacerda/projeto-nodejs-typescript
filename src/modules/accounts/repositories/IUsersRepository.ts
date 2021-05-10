d0c1c4c5d5dc7ba29c42f326de404460d08248cb
joao pedro <joao.pedro_hartbgroup@prestador.globo>
2021-05-10 17:24:00 -0300

    add validation if userAlreadyexist

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,6 +1,8 @@
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
+import { User } from '../entities/User';
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(data: string): Promise<User>;
 }
 export { IUsersRepository };
