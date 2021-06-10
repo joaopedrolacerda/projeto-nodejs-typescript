43f7876a6d6716070e896b236e46018827b0729c
joao pedro lacerda <joaopedro.lacerda33@gmail.com>
2021-06-10 09:49:32 -0300

    add list cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import { Router } from 'express';
 import { ensureAdmin } from '../middleware/ensureAdmin';
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated';
@@ -6,12 +7,13 @@ import { ensureAuthenticated } from '../middleware/ensureAuthenticated';
 const carsRoutes = Router();
 
 let createCarController = new CreateCarController();
-
+let listAvailableCars = new ListAvailableCarsController();
 carsRoutes.post(
   '/',
   ensureAuthenticated,
   ensureAdmin,
   createCarController.handle
 );
+carsRoutes.get('/available', listAvailableCars.handle);
 
 export { carsRoutes };
