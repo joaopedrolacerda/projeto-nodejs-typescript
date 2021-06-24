abd7708fd4c338c1269c1edd3fa5916b9fa077ea
joao pedro lacerda <joaopedro.lacerda33@gmail.com>
2021-06-24 09:28:33 -0300

    update user id

--- src/shared/infra/typeorm/migrations/1620266578689-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1620266578689-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1620266578689 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
